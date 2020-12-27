6c8df716bf4fc369ee8bcdea8d9df000193ec25f
ayersh009 <69464309+ayersh009@users.noreply.github.com>
2020-12-27 15:05:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.use(function(req, res, next) {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: 'Sheetapi Running Successfully'
+  });
+})
+
 // get api
 app.get('/api', api);
 
